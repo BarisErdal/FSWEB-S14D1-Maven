d8fdeebca076be613002574de318157608ab1fc2
BarisErdal <bariserdal.bme@gmail.com>
2026-02-23 19:20:44 +0300

    ii

--- src/main/java/com/workintech/cylinder/Circle.java
+++ src/main/java/com/workintech/cylinder/Circle.java
@@ -5,20 +5,14 @@ public class Circle {
     private double radius;
 
     public Circle(double radius) {
-        if (radius < 0) {
-            this.radius = 0;
-        } else {
-            this.radius = radius;
-        }
+        this.radius = radius;
     }
 
     public double getRadius() {
-
         return radius;
     }
 
     public double getArea() {
-
-        return radius*radius*Math.PI;
+        return Math.PI * radius * radius;
     }
 }
